cf999dedc1b9a1e15f37d2e02fcc0b11a008c22e
Kyle Pharoah <pharoahkyle@gmail.com>
2023-08-18 17:58:56 +0200

    init graphs

--- NovaApp/Views/FundsPage.xaml.cs
+++ NovaApp/Views/FundsPage.xaml.cs
@@ -1,9 +1,37 @@
-namespace NovaApp.Views;
+using Microcharts;
+using SkiaSharp;
 
-public partial class FundsPage : ContentView
+namespace NovaApp.Views
 {
-	public FundsPage()
-	{
-		InitializeComponent();
-	}
+    public partial class FundsPage : ContentView
+    {
+        public FundsPage()
+        {
+            InitializeComponent();
+
+            // Create chart entries for funds received
+            ChartEntry[] receivedEntries = new[]
+            {
+                new ChartEntry(200) { Label = "Monday", ValueLabel = "200", Color = SKColor.Parse("#00FF00") }, // Green
+                new ChartEntry(250) { Label = "Tuesday", ValueLabel = "250", Color = SKColor.Parse("#00FF00") }, // Green
+                // Add more entries for other days...
+            };
+
+            
+
+            // Create the line chart with combined entries
+            var chart = new LineChart
+            {
+                Entries = receivedEntries,
+                LineMode = LineMode.Straight,
+                LabelOrientation = Orientation.Horizontal // Set label orientation to horizontal
+            };
+
+            // Customize chart appearance
+            chart.LabelTextSize = 25f; // Adjust font size
+
+            // Assign the chart to your ChartView
+            chartView.Chart = chart;
+        }
+    }
 }
