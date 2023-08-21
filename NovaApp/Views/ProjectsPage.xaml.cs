fbbda2d12b93aea4cafc5bbad56486486519466a
CaidynGinger <91122965+CaidynGinger@users.noreply.github.com>
2023-08-21 17:14:47 +0200

    added get projects call to backend

--- NovaApp/Views/ProjectsPage.xaml.cs
+++ NovaApp/Views/ProjectsPage.xaml.cs
@@ -1,11 +1,36 @@
+using System;
+using System.Net.Http;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
 
 namespace NovaApp.Views;
 
 public partial class ProjectsPage : ContentView
 {
+    static string BaseUrl = "http://localhost:3000";
+    static HttpClient client = new HttpClient();
     public ProjectsPage()
     {
 
         InitializeComponent();
+        Debug.WriteLine("Projects lanched");
+        Task.Run(async () => await GetProjects());
+
+    }
+
+    public static async Task GetProjects()
+    {
+        HttpResponseMessage response = await client.GetAsync(new Uri(new Uri(BaseUrl), "/projects"));
+        if (response.IsSuccessStatusCode)
+        {
+            string responseBody = await response.Content.ReadAsStringAsync();
+            // Process responseBody here
+            Debug.WriteLine(responseBody);
+        }
+        else
+        {
+            Debug.WriteLine("Request failed");
+        }
     }
 }
