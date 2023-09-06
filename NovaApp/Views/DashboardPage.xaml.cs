5526b3063a3dc372e1622a463279a74efd565924
anchenayres <21100284@virtualwindow.co.za>
2023-09-06 02:48:34 +0200

    Dashboard - Funds
    * Graph added to dashboard page
    * Calendar wasn't working properly

--- NovaApp/Views/DashboardPage.xaml.cs
+++ NovaApp/Views/DashboardPage.xaml.cs
@@ -15,6 +15,7 @@ namespace NovaApp.Views
     public partial class DashboardPage : ContentView //Client Total
     {
 
+        private ChartEntry[] receivedEntries;
         private DashboardViewModel viewModel;
 
         private ProjectsViewModel ProjectsViewModel;
@@ -33,6 +34,35 @@ namespace NovaApp.Views
             ProjectsViewModel = new ProjectsViewModel(new Services.ProjectsRestService());
             BindingContext = ProjectsViewModel;
             LoadProjects();
+
+            receivedEntries = new[]
+            {
+                new ChartEntry(200) { Label = "Jan", ValueLabel = "200", Color = SKColor.Parse("#5BDA8C") },
+                new ChartEntry(250) { Label = "Feb", ValueLabel = "250", Color = SKColor.Parse("#5BDA8C") },
+                new ChartEntry(400) { Label = "Mar", ValueLabel = "400", Color = SKColor.Parse("#5BDA8C") },
+                new ChartEntry(300) { Label = "Apr", ValueLabel = "300", Color = SKColor.Parse("#5BDA8C") },
+                new ChartEntry(190) { Label = "May", ValueLabel = "190", Color = SKColor.Parse("#5BDA8C") },
+                new ChartEntry(600) { Label = "Jun", ValueLabel = "600", Color = SKColor.Parse("#5BDA8C") },
+                new ChartEntry(110) { Label = "Jul", ValueLabel = "110", Color = SKColor.Parse("#5BDA8C") },
+                new ChartEntry(120) { Label = "Aug", ValueLabel = "120", Color = SKColor.Parse("#5BDA8C") },
+                new ChartEntry(130) { Label = "Sep", ValueLabel = "130", Color = SKColor.Parse("#5BDA8C") },
+                new ChartEntry(140) { Label = "Oct", ValueLabel = "140", Color = SKColor.Parse("#5BDA8C") },
+                new ChartEntry(150) { Label = "Nov", ValueLabel = "150", Color = SKColor.Parse("#5BDA8C") },
+                new ChartEntry(160) { Label = "Dec", ValueLabel = "160", Color = SKColor.Parse("#5BDA8C") }
+            };
+
+            var chart = new LineChart
+            {
+                Entries = receivedEntries,
+                LineMode = LineMode.Straight,
+                LabelOrientation = Orientation.Horizontal
+            };
+
+            // Change appearance (optional)
+            chart.LabelTextSize = 12f;
+
+            // Assign chart to ChartView
+            chartView.Chart = chart;
         }
 
         public async void LoadProjects()
